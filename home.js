f8df6455449dca5250308c5d304badb56c689989
Uchey <ucheuzochukwumark@gmail.com>
2019-09-20 04:40:38 +0100

    chore: implement patient search

--- home.js
+++ home.js
@@ -49,6 +49,15 @@ function addPatients(element){
     diagnosisDescription: diagnosisDescription,
  };
 
+ if (name.length <  3) {
+  alert("Name must be 3 characters long");
+} else if (address === '') {
+  alert("Address field is required");
+} else if (Number(age) > 150 ) {
+  alert("Please enter the a valid age bracket");
+} else if ( PhoneNumber.match(/\d/g).length !== 11 || '') {
+  alert("PhoneNumber must be a valid 11 digits");
+} else {
    let URL = 'http://localhost:3000/patients';
    fetch(URL, {
        method: 'POST',
@@ -61,7 +70,7 @@ function addPatients(element){
          .then(res => res.json())
          .then(ui.displayMessage("Patient Added Succesfully", "success"));
          element.preventDefault();
-        
+  }       
 }
 
 
@@ -78,7 +87,7 @@ function dataDisplay(){
     <td>${patient.age}</td>
     <td>${patient.PhoneNumber}</td>
     <td>${patient.diagnosisDescription}</td>
-    <td class="text-center"><button id="" class="d-none d-sm-inline btn btn-sm btn-warning shadow-sm update main-color-bg" data=${data.indexOf(patient)}>Update</button></td>
+    <td class="text-center"><button id="" class="d-none d-sm-inline btn btn-sm btn-warning shadow-sm update main-color-bg" onclick="updatePatient(${patient.id}")>Update</button></td>
     <td class="text-center"><button  class="d-none d-sm-inline btn btn-sm btn-danger shadow-sm remove main-color-bg tableWist type="button" data-target="#deleteModal" data-toggle="modal" onclick="deletePatient(${patient.id})">Delete</button></td>
     </tr>
    `
@@ -100,3 +109,88 @@ function deletePatient(id){
   .catch(err => console.log(err))
   } 
 }
+
+function updatePatient(e, id){
+  e.preventDefault();
+  if(id){
+    let url = `http://localhost:3000/patients/${id}`
+    let insertPage = document.querySelector('#putModal');
+    fetch(url)
+    .then(res => {
+      return res.json();
+    }).then( file => {
+      insertPage.innerHTML=`
+    <div class="modal fade" id="updateModal" role="dialog">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+            <button type="button" class="btn  btn-lg btn-block text-capitalize">Edit Patient</button>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="modal-body" id="ref">
+        <form id="form-selector">
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="clientName">Name</label>
+            <input type="text" class="form-control" id="clientName2" value = "${file.name}"  placeholder="name>
+          </div>
+          <div class="form-group col-md-6">
+            <label for="address">Address</label>
+            <input type="text" class="form-control" id="address2" value = "${file.address}" placeholder="address">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="clientAge">Age</label>
+          <input type="text" class="form-control" id="clientAge2"  value = "${file.age}" placeholder="Age">
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-2">
+            <label for="clientPhoneNumber">PhoneNumber</label>
+            <input type="text" class="form-control" value = "${file.PhoneNumber}" id="clientPhoneNumber2">
+          </div>
+          <div class="form-group col-md-6">
+            <label for="diagnosisDescription">diagnosisDescription</label>
+              <input class="form-control" type="text"  value = "${patient.diagnosisDescription}"  id="diagnosisDescription2">
+          </div>
+          
+        <button type="button" id="updateBtn" class="btn btn-primary">Update Patient Data</button>
+      </form>
+  
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+            </div>
+        </div>
+    `;
+  
+    let updateButton = document.querySelector('#updateBtn');
+    updateButton.addEventListener('click', e => {
+     document.querySelector('#form-selector').remove();
+
+     let name = document.querySelector('#client-name').value;
+     let address = document.querySelector('#address').value;
+     let age = document.querySelector("#client-age").value;
+     let PhoneNumber = document.querySelector("#phone-number").value;
+     let diagnosisDescription = document.querySelector('#diagnosis-description').value;
+ 
+     let data = {
+     name: name,
+     address: address,
+     age: age,
+     PhoneNumber: PhoneNumber,
+     diagnosisDescription: diagnosisDescription,
+    };
+    fetch(url, {
+      method: 'PATCH',
+      body: JSON.stringify(data),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .then(ui.displayMessage("patient succesfully added", "success"));
+        });
+    });
+  };
+};
